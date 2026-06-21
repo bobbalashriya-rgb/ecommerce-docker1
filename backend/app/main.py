446d7a836a142e6e7b54eb0954d71807e2d04843
bobbalashriya-rgb <bobbalashriya@gmail.com>
2026-06-21 18:32:16 +0530

    Update app host IP address
    Changed the host IP address for the app to run on.

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,4 +6,4 @@ from . import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='54.90.101.233', port=5000, debug=True)
